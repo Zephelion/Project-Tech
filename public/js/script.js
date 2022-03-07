bc8ff466b6bf6223c209bbdbc62eb520440cb6d8
Zephelion <russellnumo@outlook.com>
2022-03-07 13:31:18 +0100

    some client side javascript

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,9 @@
-console.log("wagwan people");
+const signIn = document.getElementById('secondary');
+const section = document.querySelector('.options');
+
+console.log(section);
+
+signIn.addEventListener('click', () => {
+    section.classList.add('fade');
+    console.log("nu moet de section extenden");
+})
