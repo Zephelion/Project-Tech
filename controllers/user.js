4d4c1678ee2b81640a6eb350dedef1c354f503d0
Zephelion <russellnumo@outlook.com>
2022-03-08 23:35:26 +0100

    created user controller with store function

---
+++ controllers/user.js
@@ -0,0 +1,31 @@
+const User = require('../models/User');
+
+const storeUser = (req,res) => {
+    
+    // if(req.body.password == req.body.confirmpassword){
+
+    //     const form = {
+    //         firstname: req.body.firstname,
+    //         lastname: req.body.lastname,
+    //         age: req.body.age,
+    //         email: req.body.email,
+    //         password: req.body.password,
+    //     }
+
+    //     const user = new User(form);
+    //     user.save(function(err){
+    //         if(err) return handleError(err);
+    //     });
+
+        // res.status(200).render('step', {
+        //     form: form,
+        // })
+
+        res.render('step');
+
+    }
+
+
+module.exports = {
+    store: storeUser 
+}
