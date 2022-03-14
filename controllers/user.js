000af3bdee088d723f85bf1f39d366cbc64ffa90
Zephelion <russellnumo@outlook.com>
2022-03-14 12:09:50 +0100

    user gets stored in db

--- controllers/user.js
+++ controllers/user.js
@@ -2,27 +2,32 @@ const User = require('../models/User');
 
 const storeUser = (req,res) => {
     
-    // if(req.body.password == req.body.confirmpassword){
+    if(req.body.password == req.body.confirmpassword){
 
-    //     const form = {
-    //         firstname: req.body.firstname,
-    //         lastname: req.body.lastname,
-    //         age: req.body.age,
-    //         email: req.body.email,
-    //         password: req.body.password,
-    //     }
+        const form = {
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            age: req.body.age,
+            email: req.body.email,
+            password: req.body.password,
+        }
 
-    //     const user = new User(form);
-    //     user.save(function(err){
-    //         if(err) return handleError(err);
-    //     });
+        const user = new User(form);
+        user.save(function(err){
+            // if(err) return handleError(err);
+        });
 
-        // res.status(200).render('step', {
-        //     form: form,
-        // })
+        res.redirect('/create');
 
-        res.render('step');
+    }
+}
 
+const passUser = (req,res) =>{
+    const userInfo = req.query;
+
+    res.render('step', {
+        info:userInfo,
+    })
 }
 
 const fetchUsers = (req,res) => {
@@ -36,5 +41,6 @@ const fetchUsers = (req,res) => {
 
 module.exports = {
     store: storeUser,
-    fetch: fetchUsers
+    fetch: fetchUsers,
+    pass: passUser
 }
