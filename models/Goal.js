b1fa7cb840f842c9c11c1845a7b4f4944ff3b22b
Zephelion <russellnumo@outlook.com>
2022-04-11 19:17:22 +0200

    linter

--- models/Goal.js
+++ models/Goal.js
@@ -2,13 +2,13 @@ const  mongoose  = require("mongoose");
 
 const goalSchema = new mongoose.Schema({
 
-    title:{
-        type:String,
-        required:true,
-    },
+	title:{
+		type:String,
+		required:true,
+	},
 
-})
+});
 
 const Goal = mongoose.model('Goal', goalSchema);
 
-module.exports = Goal
+module.exports = Goal;
